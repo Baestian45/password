63b31ce12133bc7dce8d84471bb195ec451952d5
Baestian45 <niemczykbastian@gmail.com>
2024-10-05 19:25:37 +0200

    epic password keine bugs

--- password_generator/main.py
+++ password_generator/main.py
@@ -1,4 +1,19 @@
-print("sinvoll")
-print("test")
+import random
+import string
 
 
+def generated_password(characters):
+    alle_zeichen = string.ascii_letters + string.digits + string.punctuation
+    password = ""
+    for _ in range(characters):
+        password_stelle = random.randint(0, len(alle_zeichen)-1)
+        password_neues_zeichen = alle_zeichen[password_stelle]
+        password = password + password_neues_zeichen
+    return password
+
+
+print("\nWelcome to the PyPassword Generator!")
+characters = int(input("How many characters would you like in your password?: "))
+
+password = generated_password(characters)
+print(f"\ndein neues password ist '{password}'\n")
